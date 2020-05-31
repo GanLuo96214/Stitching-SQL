f22866cb671183c46aaf9b054b1675983f0b0420
gl <47106040@qq.com>
2020-05-31 20:33:30 +0800

    remove validate SQLTable and SQLField

--- validate.go
+++ validate.go
@@ -15,13 +15,13 @@ func init() {
 		log.Fatal(err)
 	}
 
-	if err := validate.RegisterValidation("sql_table_not_return_empty_string", sql_table_not_return_empty_string); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := validate.RegisterValidation("sql_field_not_return_empty_string", sql_field_not_return_empty_string); err != nil {
-		log.Fatal(err)
-	}
+	//if err := validate.RegisterValidation("sql_table_not_return_empty_string", sql_table_not_return_empty_string); err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//if err := validate.RegisterValidation("sql_field_not_return_empty_string", sql_field_not_return_empty_string); err != nil {
+	//	log.Fatal(err)
+	//}
 
 	if err := validate.RegisterValidation("where_items_array_structure", where_items_array_structure); err != nil {
 		log.Fatal(err)
