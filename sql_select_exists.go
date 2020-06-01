cf78d0de200fb2dc7c36c3d78bb09c54d2836711
gl <47106040@qq.com>
2020-06-01 12:24:37 +0800

    add sql select_exists

---
+++ sql_select_exists.go
@@ -0,0 +1,68 @@
+package StitchingSQLGo
+
+/*
+postgres https://www.postgresql.org/docs/current/sql-select.html
+*/
+type SelectExists struct {
+	SQLFields `validate:"required"`
+	Table     `validate:"required"`
+	Where
+	OrderBy
+	Limit
+	Offset
+	For
+}
+
+func (slt SelectExists) SQL() (string, []interface{}, error) {
+	if err := validate.Struct(slt); err != nil {
+		return "", nil, err
+	}
+
+	s := SqlBuilder{}
+
+	// select exists(select
+	s.WriteString("select exists(select")
+
+	// field1, field2
+	if err := slt.SQLFields.sqlFields(&s); err != nil {
+		return "", nil, err
+	}
+
+	// from
+	s.WriteString(" from")
+
+	// table
+	if err := slt.Table.Table(&s); err != nil {
+		return "", nil, err
+	}
+
+	// where field1 = value1 and filed2 = value2
+	if err := slt.Where.where(&s); err != nil {
+		return "", nil, err
+	}
+
+	// order by field1 asc, field2 desc
+	if err := slt.OrderBy.orderBy(&s); err != nil {
+		return "", nil, err
+	}
+
+	// limit X
+	if err := slt.Limit.limit(&s); err != nil {
+		return "", nil, err
+	}
+
+	// offset X
+	if err := slt.Offset.offset(&s); err != nil {
+		return "", nil, err
+	}
+
+	// )
+	s.WriteByte(')')
+
+	// for update
+	if err := slt.For.For(&s); err != nil {
+		return "", nil, err
+	}
+
+	return s.String(), s.args, nil
+}
---
+++ sql_select_exists_test.go
@@ -0,0 +1,100 @@
+package StitchingSQLGo
+
+import "testing"
+
+//go:generate stitching_sql -type=TestSelectExists_SQLTable -file-name=sql_select_exists_table_stitching_sql_test.go -is-add-import=false
+type TestSelectExists_SQLTable struct {
+	Field1 int
+	Field2 int
+	Field3 int
+}
+
+func TestSelectExists_SQL(t *testing.T) {
+
+	s := SelectExists{
+		SQLFields: SQLFields{
+			TestSelectExists_SQLTable_F.Field1,
+			TestSelectExists_SQLTable_F.Field2,
+			TestSelectExists_SQLTable_F.Field3,
+		},
+		Table: TestSelectExists_SQLTable{},
+		Where: Where{
+			Condition{
+				L: TestSelectExists_SQLTable_F.Field1,
+				O: EQ,
+				R: 1,
+			},
+			AND,
+			WhereItems{
+				Condition{
+					L: TestSelectExists_SQLTable_F.Field1,
+					O: EQ,
+					R: 2,
+				},
+				AND,
+				Condition{
+					L: TestSelectExists_SQLTable_F.Field2,
+					O: NEQ,
+					R: 3,
+				},
+				AND,
+				Condition{
+					L: TestSelectExists_SQLTable_F.Field2,
+					O: GT,
+					R: 4,
+				},
+				AND,
+				WhereItems{
+					Condition{
+						L: TestSelectExists_SQLTable_F.Field1,
+						O: EQ,
+						R: 5,
+					},
+					AND,
+					Condition{
+						L: TestSelectExists_SQLTable_F.Field2,
+						O: NEQ,
+						R: 6,
+					},
+					AND,
+					Condition{
+						L: TestSelectExists_SQLTable_F.Field2,
+						O: GT,
+						R: 7,
+					},
+				},
+			},
+		},
+		OrderBy: map[Field]OrderByKind{
+			TestSelectExists_SQLTable_F.Field1: ASC,
+			TestSelectExists_SQLTable_F.Field2: DESC,
+			TestSelectExists_SQLTable_F.Field3: ASC,
+		},
+		Limit:  1,
+		Offset: 1,
+		For:    ForUpdate,
+	}
+
+	sql, args, err := s.SQL()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	excepts := map[string][]interface{}{
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field1 asc, test_select_exists__sqltable.field2 desc, test_select_exists__sqltable.field3 asc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field2 desc, test_select_exists__sqltable.field1 asc, test_select_exists__sqltable.field3 asc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field3 asc, test_select_exists__sqltable.field2 desc, test_select_exists__sqltable.field1 asc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field2 desc, test_select_exists__sqltable.field3 asc, test_select_exists__sqltable.field1 asc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field3 asc, test_select_exists__sqltable.field1 asc, test_select_exists__sqltable.field2 desc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+		"select exists(select test_select_exists__sqltable.field1, test_select_exists__sqltable.field2, test_select_exists__sqltable.field3 from test_select_exists__sqltable where test_select_exists__sqltable.field1 = $1 and ( test_select_exists__sqltable.field1 = $2 and test_select_exists__sqltable.field2 != $3 and test_select_exists__sqltable.field2 > $4 and ( test_select_exists__sqltable.field1 = $5 and test_select_exists__sqltable.field2 != $6 and test_select_exists__sqltable.field2 > $7)) order by test_select_exists__sqltable.field1 asc, test_select_exists__sqltable.field3 asc, test_select_exists__sqltable.field2 desc limit 1 offset 1) for update": {1, 2, 3, 4, 5, 6, 7},
+	}
+
+	exceptArgs, ok := excepts[sql]
+	if ok == false {
+		t.Fatalf("now\n%s", sql)
+	}
+
+	if argsCompare(exceptArgs, args) == false {
+		t.Fatalf("except\n%v\nnow\n%v", exceptArgs, args)
+	}
+}
